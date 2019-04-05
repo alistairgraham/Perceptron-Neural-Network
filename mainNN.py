296931f107152f72f922039f70a825c72f66a905
alistairisjesus <38551960+alistairisjesus@users.noreply.github.com>
2019-04-05 15:14:08 +1300

    Initial commit

---
+++ mainNN.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  5 14:54:20 2019
+
+@author: Alistair Graham
+"""
+
+import sys
+
+def loadImages(filename):
+    
+    return
+
+
+
+
+def main():
+    loadImages(argv[1])
+
+main()
+
